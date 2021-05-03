8f5bccb6e020f66fd5b3ebc73187b4cb2979ba99
soymendo <maximendoza92@hotmail.com.ar>
2021-05-03 14:10:01 -0300

    tp2

---
+++ TP-02/Entidades/Ciclomotor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ciclomotor : Vehiculo
+    {
+
+
+
+
+
+
+
+        #region  "Propiedades"
+
+        /// <summary>
+        /// Ciclomotor son 'Chico'
+        /// </summary>
+        public override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Chico;
+            }
+        }
+        #endregion
+
+
+
+        #region "Constructores"
+        public Ciclomotor(EMarca marca, string chasis, ConsoleColor color)
+            :base(chasis,marca,color)
+        {
+        }
+        #endregion
+
+
+        #region "Metodos"
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"CICLOMOTOR");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"TAMAÑO: {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+
+
+
+
+    }
+}
---
+++ TP-02/Entidades/Suv.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv : Vehiculo
+    {
+
+        #region  "Propiedades"
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        public override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+        #endregion
+
+
+
+        #region "Constructores"
+        /// <summary>
+        /// Único constructor de instancia.
+        /// </summary>
+        /// <param name="chasis">Chasis del vehículo</param>
+        /// <param name="marca">Marca del vehículo</param>
+        /// <param name="color">Color del vehículo</param>
+        public Suv(EMarca marca,string chasis,ConsoleColor color)
+        :base(chasis,marca,color)
+        {
+
+        }
+        #endregion
+
+
+        #region "Metodos"
+        /// <summary>
+        /// Muestra la informacion de la clase junto con la de la clase base
+        /// </summary>
+        /// <returns></returns>
+        public override  string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"SUV");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine($"Tamanio: {this.Tamanio}");
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+
+        #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
